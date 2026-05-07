e6b92c595c7da69d97ce6b6734cbcf40ed82ba7a
aaditi-singhh <aditisingh6841@gmail.com>
2026-05-07 20:33:57 +0530

    Add Hello World program in C++

---
+++ 1helloworld.cpp
@@ -0,0 +1,5 @@
+#include <iostream> // Required for input/output operations
+intmain(){
+std::cout << "Hello, World!" << std::endl;// Print message to the console
+return 0;// Indicate that the program ended successfully
+}
